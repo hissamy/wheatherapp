3037abc3584065afc3c404d1d37a6446e27833e5
hissamus <hissam.sherkar@gmail.com>
2018-10-07 11:22:57 +0100

    ADDED THE { sourceMap: true, minimize: true } FOR UGLIGY

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,6 @@ module.exports = {
     new webpack.DefinePlugin({
       "process.env.NODE_ENV": JSON.stringify("production")
     }),
-    new webpack.optimize.UglifyJsPlugin() 
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true, minimize: true }) 
   ]
 };
